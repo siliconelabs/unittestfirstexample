9ff548be3d02161e442c076d7a852a15e3ce6d0c
muhammet-sahin-ITU <muhammetsahin@itu.edu.tr>
2021-04-18 13:00:08 +0300

    Initial commit

---
+++ src/main/java/com/example/unittestfirstexample/UnittestfirstexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.unittestfirstexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnittestfirstexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnittestfirstexampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/unittestfirstexample/UnittestfirstexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unittestfirstexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnittestfirstexampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
